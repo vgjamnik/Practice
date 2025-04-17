e92a5ef5346e162fc209941ca42d402a601d1194
vgjamnik <vgjamnik@gmail.com>
2025-04-18 00:27:57 +0530

    Created React App

---
+++ JobilioApp/jwapp/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Jobilio App</header>
+    </div>
+  );
+}
+
+export default App;
